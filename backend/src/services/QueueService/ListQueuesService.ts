e01a7265bd4d461092429df1ad3e567932e934ad
Anderson Nascimento Mendonça <85626023+Anderson-Nascimento-Mendonca@users.noreply.github.com>
2022-07-16 16:43:41 -0300

    teste para começar

--- backend/src/services/QueueService/ListQueuesService.ts
+++
@@ -1,9 +0,0 @@
-import Queue from "../../models/Queue";
-
-const ListQueuesService = async (): Promise<Queue[]> => {
-  const queues = await Queue.findAll();
-
-  return queues;
-};
-
-export default ListQueuesService;
